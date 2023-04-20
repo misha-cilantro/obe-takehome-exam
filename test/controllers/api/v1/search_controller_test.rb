ff12c842965555e411190d9ec1b2bbe97e153fdc
Misha Velichansky <mvelichansky@gmail.com>
2023-04-20 14:25:29 -0400

    more tests

--- test/controllers/api/v1/search_controller_test.rb
+++ test/controllers/api/v1/search_controller_test.rb
@@ -7,10 +7,22 @@ class Api::V1::SearchControllerTest < ActionDispatch::IntegrationTest
 
     results = JSON.parse(response.body)
     assert results.length > 0, "should have entries"
+    
+    rick = results.first
+    assert rick["name"], "Rick Sanchez"
+    
+    assert_not_nil rick["seasons_appearing"]
+    assert_equal rick["seasons_appearing"], {
+      "1" => 11,
+      "2" => 10,
+      "3" => 10,
+      "4" => 10,
+      "5" => 10
+    }
   end
 
   test "error if name is missing" do
     get api_v1_character_path
-    assert_response :bad_request    
+    assert_response :bad_request
   end
 end
