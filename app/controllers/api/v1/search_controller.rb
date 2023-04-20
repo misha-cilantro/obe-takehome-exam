31fb88cfb4b29a5fc770bb2caa23b95d19e990f6
Misha Velichansky <mvelichansky@gmail.com>
2023-04-20 12:38:13 -0400

    basic search implemented

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::SearchController < ApplicationController
   def search
-    render json: {
-      ok: "ok"
-    }
+    name = params[:name]
+    
+    characters = Rickmorty::Character.new
+    render json: characters.search(name)
   end
 end
